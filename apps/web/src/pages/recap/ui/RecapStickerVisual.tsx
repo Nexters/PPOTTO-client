4fb923529cd5d3bbca1f4b0610f3e3523c9e1ab0
cchaeyoung <kimchaeyoung05@naver.com>
2026-07-31 04:38:45 +0900

    ✨ Feat: 스티커 컷아웃에 흰색 테두리 효과 적용

--- apps/web/src/pages/recap/ui/RecapStickerVisual.tsx
+++ apps/web/src/pages/recap/ui/RecapStickerVisual.tsx
@@ -11,8 +11,26 @@ type RecapStickerVisualProps = {
 export function RecapStickerVisual({ imageUrl, floatComments }: RecapStickerVisualProps) {
   return (
     <div className="relative flex h-52 w-full items-center justify-center">
+      <svg width="0" height="0" className="absolute">
+        <filter id="sticker-outline">
+          <feMorphology in="SourceAlpha" operator="dilate" radius="3" result="dilated" />
+          <feFlood floodColor="white" result="color" />
+          <feComposite in="color" in2="dilated" operator="in" result="outline" />
+          <feMerge>
+            <feMergeNode in="outline" />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+      </svg>
       <div className="relative h-44 w-44">
-        <Image src={imageUrl} alt="" fill sizes="176px" className="object-contain" />
+        <Image
+          src={imageUrl}
+          alt=""
+          fill
+          sizes="176px"
+          className="object-contain"
+          style={{ filter: 'url(#sticker-outline)' }}
+        />
       </div>
       {floatComments.map((comment) => (
         <div
