cd1dcba4063fe44729c509084cac59090cfa817e
cchaeyoung <kimchaeyoung05@naver.com>
2026-07-31 21:01:13 +0900

    ✨ Feat: 리캡 사진 그리드에서 확대뷰로 진입 연결

--- apps/web/src/pages/recap/RecapPage.tsx
+++ apps/web/src/pages/recap/RecapPage.tsx
@@ -45,7 +45,7 @@ export function RecapPage({ stickerId }: RecapPageProps) {
         </div>
         <RecapThemeTags tags={tags.map((tag) => tag.content)} />
       </div>
-      <RecapPhotoGrid photos={data.photos} />
+      <RecapPhotoGrid stickerId={stickerId} photos={data.photos} />
     </div>
   );
 }
