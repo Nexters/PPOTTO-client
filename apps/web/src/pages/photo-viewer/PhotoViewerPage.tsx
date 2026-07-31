3a49ad31360d6bd2e81104110507ef911c166d3a
cchaeyoung <kimchaeyoung05@naver.com>
2026-07-31 18:54:51 +0900

    🔧 Chore: 사진 크게 보기 화면 라우팅 뼈대 추가

---
+++ apps/web/src/pages/photo-viewer/PhotoViewerPage.tsx
@@ -0,0 +1,11 @@
+type PhotoViewerPageProps = {
+  stickerId: string;
+  initialIndex: string;
+};
+
+export function PhotoViewerPage({
+  stickerId: _stickerId,
+  initialIndex: _initialIndex,
+}: PhotoViewerPageProps) {
+  return <div>사진 크게 보기</div>;
+}
