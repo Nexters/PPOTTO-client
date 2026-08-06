efbaf440b4ee27353f48bd8c73c038d592244f64
kimyounghee425 <doctorsean0425@gmail.com>
2026-08-06 21:49:47 +0900

    ✨ Feat: 보드 목록 조회 API 추가

--- apps/web/src/entities/board/api/board-queries.ts
+++ apps/web/src/entities/board/api/board-queries.ts
@@ -3,6 +3,12 @@ import { useQuery } from '@tanstack/react-query';
 import { boardApi } from './board-api';
 import { boardQueryKeys } from './board-query-keys';
 
+export const useBoardListQuery = () =>
+  useQuery({
+    queryKey: boardQueryKeys.list(),
+    queryFn: boardApi.list,
+  });
+
 export const useBoardQuery = (boardId: string) =>
   useQuery({
     queryKey: boardQueryKeys.detail(boardId),
