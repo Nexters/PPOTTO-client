4bad8adbb5249fc3c70bdcc086bed9a268955522
kimyounghee425 <doctorsean0425@gmail.com>
2026-08-06 16:20:36 +0900

    ✨ Feat: 카카오 뱃지 아이콘 추가
    설정 화면 계정 행에 쓰는 노란 원형 뱃지. 기존 KakaoLogo는 말풍선만 있는
    로그인 버튼용이라 글리프가 달라 Figma 에셋을 그대로 받아 추가한다.

--- packages/assets/generated/index.ts
+++ packages/assets/generated/index.ts
@@ -11,6 +11,7 @@ export { default as IconPlus } from './IconPlus';
 export { default as IconSettings } from './IconSettings';
 export { default as IconText } from './IconText';
 export { default as ImageMultiple } from './ImageMultiple';
+export { default as KakaoBadge } from './KakaoBadge';
 export { default as KakaoLogo } from './KakaoLogo';
 export { default as Logo } from './Logo';
 export { default as Share } from './Share';
