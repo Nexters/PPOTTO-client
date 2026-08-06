7a20f1690bb9690533549b905d68ae09f306bc2d
kimyounghee425 <doctorsean0425@gmail.com>
2026-08-06 16:20:23 +0900

    ♻️ Refactor: 에셋 생성 결과를 생성기 출력과 동기화
    Share, ImageMultiple만 웹 전용 SVGProps 형태로 남아 있어 pnpm generate를 돌리면
    매번 형태가 바뀌었다. 나머지 15개와 같은 react-native-svg 형태로 통일한다.
    
    RecapHeader는 Share에 className을 넘기고 있었는데 SvgProps에는 className이 없어
    color prop으로 교체한다.

--- packages/assets/generated/ImageMultiple.tsx
+++ packages/assets/generated/ImageMultiple.tsx
@@ -1,42 +1,20 @@
-import type { SVGProps } from 'react';
-const ImageMultiple = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
+import Svg, { G, Path, Defs } from 'react-native-svg';
+/* SVGR has dropped some elements not supported by react-native-svg: filter */
+import type { SvgProps } from 'react-native-svg';
+const ImageMultiple = (props: SvgProps) => (
+  <Svg
     width={36}
     height={39}
     fill="none"
     viewBox="0 0 36 39"
-    color="var(--icon-default-color, white)"
+    color={props.color ?? 'white'}
     {...props}
   >
-    <g fill="currentColor" filter="url(#icon-ImageMultiple__a)">
-      <path d="M24.167 11.39a3.056 3.056 0 0 0-3.056-3.056h-7.222a3.056 3.056 0 0 0-3.056 3.055v7.222a3.056 3.056 0 0 0 3.056 3.056h7.222a3.056 3.056 0 0 0 3.055-3.056z" />
-      <path d="M27.5 14.167c0-.912-.444-1.524-1.262-1.979a.833.833 0 1 0-.81 1.457c.332.184.405.285.405.522V22.5a.84.84 0 0 1-.833.834h-8.332a.84.84 0 0 1-.67-.34l-.055-.083a.832.832 0 1 0-1.448.825A2.5 2.5 0 0 0 16.667 25H25c1.377 0 2.5-1.123 2.5-2.5z" />
-    </g>
-    <defs>
-      <filter
-        id="icon-ImageMultiple__a"
-        width={38.333}
-        height={38.333}
-        x={0}
-        y={0}
-        colorInterpolationFilters="sRGB"
-        filterUnits="userSpaceOnUse"
-      >
-        <feFlood floodOpacity={0} result="BackgroundImageFix" />
-        <feColorMatrix
-          in="SourceAlpha"
-          result="hardAlpha"
-          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-        />
-        <feOffset dy={2.5} />
-        <feGaussianBlur stdDeviation={5.417} />
-        <feComposite in2="hardAlpha" operator="out" />
-        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.5 0" />
-        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_836_671" />
-        <feBlend in="SourceGraphic" in2="effect1_dropShadow_836_671" result="shape" />
-      </filter>
-    </defs>
-  </svg>
+    <G fill="currentColor" filter="url(#icon-ImageMultiple__a)">
+      <Path d="M24.167 11.39a3.056 3.056 0 0 0-3.056-3.056h-7.222a3.056 3.056 0 0 0-3.056 3.055v7.222a3.056 3.056 0 0 0 3.056 3.056h7.222a3.056 3.056 0 0 0 3.055-3.056z" />
+      <Path d="M27.5 14.167c0-.912-.444-1.524-1.262-1.979a.833.833 0 1 0-.81 1.457c.332.184.405.285.405.522V22.5a.84.84 0 0 1-.833.834h-8.332a.84.84 0 0 1-.67-.34l-.055-.083a.832.832 0 1 0-1.448.825A2.5 2.5 0 0 0 16.667 25H25c1.377 0 2.5-1.123 2.5-2.5z" />
+    </G>
+    <Defs></Defs>
+  </Svg>
 );
 export default ImageMultiple;
--- packages/assets/generated/Share.tsx
+++ packages/assets/generated/Share.tsx
@@ -1,21 +1,21 @@
-import type { SVGProps } from 'react';
-const Share = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
+import Svg, { Path } from 'react-native-svg';
+import type { SvgProps } from 'react-native-svg';
+const Share = (props: SvgProps) => (
+  <Svg
     width={16}
     height={17}
     fill="none"
     viewBox="0 0 16 17"
-    color="var(--icon-default-color, white)"
+    color={props.color ?? 'white'}
     {...props}
   >
-    <path
+    <Path
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={2}
       d="M1 11.17v3.074c0 .466.184.912.513 1.242.328.329.773.514 1.237.514h10.5c.464 0 .91-.185 1.237-.514A1.76 1.76 0 0 0 15 14.244V11.17m-6.964-.218V1m4 3.803L8.036 1l-4 3.803"
     />
-  </svg>
+  </Svg>
 );
 export default Share;
