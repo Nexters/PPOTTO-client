f1b09e684377b217b3a3548f3530a47df8bffc4d
강은우 <80903685+eunwoo66@users.noreply.github.com>
2026-08-15 14:24:34 +0900

    ♻️ Refactor: 선택 박스 시각 요소를 SelectionBoxFrame으로 분리

--- apps/web/src/pages/board/ui/SelectBox.tsx
+++ apps/web/src/pages/board/ui/SelectBox.tsx
@@ -3,17 +3,7 @@
 import type { StickerTransform } from '../model/board-transform';
 
 import { getPhotoSize, stickerZIndex, useStickerImage, type StickerData } from './Sticker';
-
-const BOX_COLOR = '#009fff';
-const TICK_SIZE = 7;
-const BORDER_WIDTH = 1.5;
-
-const TICK_POSITIONS: { top?: number; bottom?: number; left?: number; right?: number }[] = [
-  { top: -TICK_SIZE / 2, left: -TICK_SIZE / 2 },
-  { top: -TICK_SIZE / 2, right: -TICK_SIZE / 2 },
-  { bottom: -TICK_SIZE / 2, left: -TICK_SIZE / 2 },
-  { bottom: -TICK_SIZE / 2, right: -TICK_SIZE / 2 },
-];
+import { SelectionBoxFrame } from './SelectionBoxFrame';
 
 type SelectBoxProps = {
   sticker: StickerData;
@@ -32,34 +22,13 @@ export function SelectBox({ sticker, transformOverride }: SelectBoxProps) {
   const rotation = transformOverride?.rotation ?? sticker.rotation;
 
   return (
-    <div
-      style={{
-        position: 'absolute',
-        left: x,
-        top: y,
-        width,
-        height,
-        transform: `translate(-50%, -50%) rotate(${rotation}deg)`,
-        border: `${BORDER_WIDTH}px solid ${BOX_COLOR}`,
-        boxSizing: 'border-box',
-        pointerEvents: 'none',
-        zIndex: stickerZIndex(sticker) + 2,
-      }}
-    >
-      {TICK_POSITIONS.map((position, index) => (
-        <div
-          key={index}
-          style={{
-            position: 'absolute',
-            width: TICK_SIZE,
-            height: TICK_SIZE,
-            background: '#fff',
-            border: `${BORDER_WIDTH}px solid ${BOX_COLOR}`,
-            boxSizing: 'border-box',
-            ...position,
-          }}
-        />
-      ))}
-    </div>
+    <SelectionBoxFrame
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      rotation={rotation}
+      zIndex={stickerZIndex(sticker) + 2}
+    />
   );
 }
