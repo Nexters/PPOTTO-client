37179cde705007f1fdf327431e352e18f9063e1f
cchaeyoung <kimchaeyoung05@naver.com>
2026-08-21 14:45:22 +0900

    ✨ Feat: 사진 캐러셀 슬라이드 사이 여백 추가

--- apps/web/src/pages/photo-viewer/ui/PhotoCarousel.tsx
+++ apps/web/src/pages/photo-viewer/ui/PhotoCarousel.tsx
@@ -15,7 +15,7 @@ export function PhotoCarousel({ stickerId, photos, selectedIndex, onSelect }: Ph
 
   return (
     <div ref={carouselRef} className="h-full w-full overflow-hidden">
-      <div className="flex h-full">
+      <div className="flex h-full gap-2">
         {photos.map((photo, index) => (
           <div key={photo.id} className="relative h-full min-w-0 flex-[0_0_100%]">
             <StickerPhotoImage
